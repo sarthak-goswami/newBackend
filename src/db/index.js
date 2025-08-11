9b88bc07d347b4984cf8703a28928b56a62c5bf6
sarthak goswami <goswamsarthak339@gmal.com>
2025-08-11 22:51:28 +0530

    MONGODB connected successfully

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
+
 
 const connectDB = async ()=>{
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected successfully DB HOST : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("mongo db connection error : ",error);
         process.exit(1);
     }
 }
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:".env"
+})
+
+connectDB();
+
+
+
+
+
 /*
 ;(async ()=>{
     try {
